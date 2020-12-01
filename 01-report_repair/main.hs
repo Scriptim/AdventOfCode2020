d61eeb8a5ccaf53da43bb8e7422050824ed65428
Scriptim <Scriptim@gmx.de>
2020-12-01 22:46:32 +0100

    1.2

--- 01-report_repair/main.hs
+++ 01-report_repair/main.hs
@@ -1,7 +1,11 @@
-get2020addends :: [Int] -> (Int, Int)
-get2020addends xs = head $ filter (\(x, y) -> x + y == 2020) $ (,) <$> xs <*> xs
+get2020addends2 :: [Int] -> (Int, Int)
+get2020addends2 xs = head $ filter (\(x, y) -> x + y == 2020) $ (,) <$> xs <*> xs
+
+get2020addends3 :: [Int] -> (Int, Int, Int)
+get2020addends3 xs = head $ filter (\(x, y, z) -> x + y + z == 2020) $ (,,) <$> xs <*> xs <*> xs
 
 main :: IO ()
 main = do
   input <- map read . lines <$> readFile "input.txt" :: IO [Int]
-  print $ uncurry (*) $ get2020addends input
+  print $ uncurry (*) $ get2020addends2 input
+  print $ (\(x, y, z) -> x * y * z) $ get2020addends3 input
