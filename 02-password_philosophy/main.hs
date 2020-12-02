5d1ce00fa5719946a44b290f4c4ddc11e5cd4189
Scriptim <Scriptim@gmx.de>
2020-12-02 23:53:02 +0100

    2.2

--- 02-password_philosophy/main.hs
+++ 02-password_philosophy/main.hs
@@ -11,11 +11,16 @@ parse str = (min, max, char, pass)
     char = head . (!! 1) . words $ str
     pass = last . words $ str
 
-isValid :: Password -> Bool
-isValid (min, max, char, pass) = count >= min && count <= max
+isValid1 :: Password -> Bool
+isValid1 (min, max, char, pass) = count >= min && count <= max
   where count = length $ filter (== char) pass
+
+isValid2 :: Password -> Bool
+isValid2 (index1, index2, char, pass) = isChar index1 /= isChar index2
+  where isChar index = pass !! pred index == char
 
 main :: IO ()
 main = do
   input <- map parse . lines <$> readFile "input.txt"
-  print $ length $ filter isValid input
+  print $ length $ filter isValid1 input
+  print $ length $ filter isValid2 input
