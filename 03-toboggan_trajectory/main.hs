13c71429be0a293548c18e2dc21414f87be6cb72
Scriptim <Scriptim@gmx.de>
2020-12-03 11:36:25 +0100

    3.2

--- 03-toboggan_trajectory/main.hs
+++ 03-toboggan_trajectory/main.hs
@@ -1,9 +1,14 @@
-rowsWithTrees :: Int -> [[Bool]] -> [Bool]
-rowsWithTrees _ [] = []
-rowsWithTrees i (x:xs) = (x !! pos):rowsWithTrees (i + 1) xs
-  where pos = (i * 3) `mod` length x
+rowsWithTrees :: Int -> Int -> Int -> [[Bool]] -> [Bool]
+rowsWithTrees _ _ _ [] = []
+rowsWithTrees right down i (x:xs) = (x !! pos):rowsWithTrees right down (i + 1) (drop (down - 1) xs)
+  where pos = (i * right) `mod` length x
+
+countTrees :: [Bool] -> Int
+countTrees = length . filter id
 
 main :: IO ()
 main = do
   input <- map (map (=='#')) . lines <$> readFile "input.txt" :: IO [[Bool]]
-  print $ length $ filter id $ rowsWithTrees 0 input
+  print $ countTrees $ rowsWithTrees 3 1 0 input
+  let slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+  print $ product $ map (\(r, d) -> countTrees $ rowsWithTrees r d 0 input) slopes
