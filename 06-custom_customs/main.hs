87d91f8dc05a7d341910645c91197b36c04eaeb9
Scriptim <Scriptim@gmx.de>
2020-12-06 12:50:32 +0100

    6.2

--- 06-custom_customs/main.hs
+++ 06-custom_customs/main.hs
@@ -1,10 +1,14 @@
-import           Data.List       (nub)
+import           Data.List       (intersect, nub)
 import           Data.List.Split (splitOn)
 
-getAnswers :: String -> String
-getAnswers = nub . concat . lines
+getAnswers1 :: String -> String
+getAnswers1 = nub . concat . lines
+
+getAnswers2 :: String -> String
+getAnswers2 = foldr1 intersect . lines
 
 main :: IO ()
 main = do
   input <- splitOn "\n\n" <$> readFile "input.txt"
-  print $ sum $ map (length . getAnswers) input
+  print $ sum $ map (length . getAnswers1) input
+  print $ sum $ map (length . getAnswers2) input
