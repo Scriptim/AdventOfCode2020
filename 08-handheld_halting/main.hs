b2723f6b9440f1b4013cb9a34c13232979fe4d0e
Scriptim <Scriptim@gmx.de>
2020-12-08 11:11:54 +0100

    8.2

--- 08-handheld_halting/main.hs
+++ 08-handheld_halting/main.hs
@@ -1,10 +1,20 @@
-eval :: [(String, Int)] -> [Int] -> Int -> Int -> Int
+eval :: [(String, Int)] -> [(Int, Int)] -> Int -> Int -> ([(Int, Int)], Bool)
 eval instr history accu index
-  | index `elem` history = accu
+  | index `elem` map snd history = (history, False)
+  | index >= length instr = (history, True)
   | otherwise = let (op, arg) = instr !! index in case op of
-    "nop" -> eval instr (index:history) accu (succ index)
-    "acc" -> eval instr (index:history) (accu + arg) (succ index)
-    "jmp" -> eval instr (index:history) accu (index + arg)
+    "nop" -> eval instr ((accu, index):history) accu (succ index)
+    "acc" -> eval instr ((accu, index):history) (accu + arg) (succ index)
+    "jmp" -> eval instr ((accu, index):history) accu (index + arg)
+
+replaceInstr :: [(String, Int)] -> [(Int, Int)] -> [[(String, Int)]]
+replaceInstr _ [] = []
+replaceInstr instr ((_, index):history)
+  | op == "acc" = replaceInstr instr history
+  | otherwise = replacedInstr:replaceInstr instr history
+    where
+      (op, arg) = instr !! index
+      replacedInstr = let (xs,_:ys) = splitAt index instr in xs ++ [(if op == "nop" then "jmp" else "nop", arg)] ++ ys
 
 parseInput :: String -> [(String, Int)]
 parseInput = map (toTuple . words) . lines
@@ -13,4 +23,6 @@ parseInput = map (toTuple . words) . lines
 main :: IO ()
 main = do
   input <- parseInput <$> readFile "input.txt"
-  print $ eval input [] 0 0
+  let history = eval input [] 0 0
+  print $ fst . head . fst $ history
+  print $ fst . head . fst . head . filter snd . map (\instr -> eval instr [] 0 0) $ replaceInstr input $ fst history
