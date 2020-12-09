933a78286956118ab97c70da391f9136e68e6970
Scriptim <Scriptim@gmx.de>
2020-12-09 13:58:33 +0100

    9.2

--- 09-encoding_error/main.hs
+++ 09-encoding_error/main.hs
@@ -4,7 +4,16 @@ findInvalid prev@(_:prevTail) (current:next)
   | otherwise = current
     where isValid = any (\(a, b) -> a /= b && a + b == current) $ (,) <$> prev <*> prev
 
+subsequence :: Int -> [Int] -> [Int]
+subsequence n xs
+  | length sums > 1 && last sums == n = take (length sums) xs
+  | otherwise = subsequence n (tail xs)
+    where sums = takeWhile (<= n) $ scanl1 (+) xs
+
 main :: IO ()
 main = do
   input <- map read . lines <$> readFile "input.txt" :: IO [Int]
-  print $ findInvalid (take 25 input) (drop 25 input)
+  let invalid = findInvalid (take 25 input) (drop 25 input)
+  print invalid
+  let addends = subsequence invalid input
+  print $ minimum addends + maximum addends
