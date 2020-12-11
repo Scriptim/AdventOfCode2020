9cb895bc58d082c3b507192f9df7699282c46846
Scriptim <Scriptim@gmx.de>
2020-12-11 11:25:43 +0100

    11.2

--- 11-seating_system/main.hs
+++ 11-seating_system/main.hs
@@ -1,22 +1,31 @@
 import           Control.Monad (guard)
 
-neighbors :: [String] -> Int -> Int -> [Char]
-neighbors xs i j = do
-  (i', j') <- [(i-1, j-1), (i-1, j), (i-1, j+1), (i, j-1), (i, j+1), (i+1, j-1), (i+1, j), (i+1, j+1)]
-  guard $ i' >= 0 && i' < length xs && j' >= 0 && j' < length (head xs)
+inBounds :: [String] -> Int -> Int -> Bool
+inBounds xs i j = i >= 0 && i < length xs && j >= 0 && j < length (head xs)
+
+neighbors :: Bool -> [String] -> Int -> Int -> [Char]
+neighbors lookFar xs i j = do
+  (iDir, jDir) <- [(-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1)]
+  let goDir (i', j') = (i' + iDir, j' + jDir)
+      (i', j') = if lookFar
+        then until (\(i', j') -> not (inBounds xs i' j') || xs !! i' !! j' /= '.') goDir (goDir (i, j))
+        else goDir (i, j)
+  guard $ inBounds xs i' j'
   return $ xs !! i' !! j'
 
-newState :: [String] -> Int -> Int -> Char
-newState xs i j
+newState :: ([String] -> Int -> Int -> [Char]) -> Int -> [String] -> Int -> Int -> Char
+newState neighbors threshold xs i j
   | xs !! i !! j == 'L' && occupied == 0 = '#'
-  | xs !! i !! j == '#' && occupied >= 4 = 'L'
+  | xs !! i !! j == '#' && occupied >= threshold = 'L'
   | otherwise = xs !! i !! j
     where occupied = length . filter (=='#') $ neighbors xs i j
 
-nextCycle :: [String] -> [String]
-nextCycle xs = [[newState xs i j | j <- [0..pred . length . head $ xs]] | i <- [0..pred . length $ xs]]
+nextCycle :: ([String] -> Int -> Int -> Char) -> [String] -> [String]
+nextCycle newState xs = [[newState xs i j | j <- [0..pred . length . head $ xs]] | i <- [0..pred . length $ xs]]
 
 main :: IO ()
 main = do
   input <- lines <$> readFile "input.txt"
-  print $ length . filter (=='#') . concat $ (until =<< ((==) =<<)) nextCycle input
+  let countFinalOccupied = length . filter (=='#') . concat
+  print $ countFinalOccupied $ (until =<< ((==) =<<)) (nextCycle $ newState (neighbors False) 4) input
+  print $ countFinalOccupied $ (until =<< ((==) =<<)) (nextCycle $ newState (neighbors True) 5) input
