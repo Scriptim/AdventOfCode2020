d7a1193fbd53044c40823b08c8316143e7956ef9
Scriptim <Scriptim@gmx.de>
2020-12-13 21:19:07 +0100

    13.2

--- 13-shuttle_search/main.hs
+++ 13-shuttle_search/main.hs
@@ -1,17 +1,26 @@
+import           Data.Bifunctor  (first)
 import           Data.List       (minimumBy)
 import           Data.List.Split (splitOn)
-import           Data.Maybe      (fromJust)
+import           Data.Maybe      (fromJust, isJust)
 import           Data.Ord        (comparing)
 import           Text.Read       (readMaybe)
 
-readInput :: [String] -> (Int, [Maybe Int])
+readInput :: [String] -> (Integer, [Maybe Integer])
 readInput [startTime, busIds] = (read startTime, map readMaybe $ splitOn "," busIds)
 
-findEarliestBus :: Int -> [Int] -> (Int, Int)
+findEarliestBus :: Integer -> [Integer] -> (Integer, Integer)
 findEarliestBus startTime xs = minimumBy (comparing snd) $ zip xs (map firstDepart xs)
   where firstDepart x = ceiling (fromIntegral startTime / fromIntegral x) * x
+
+getOffsets :: [Maybe Integer] -> [(Integer, Integer)]
+getOffsets xs = map (first fromJust) $ filter (\(id, _) -> isJust id) $ zip xs [0..]
+
+findTimestamp :: (Integer, Integer) -> (Integer, Integer) -> (Integer, Integer)
+findTimestamp (accu1, accu2) (busId, offset) = (accu1 * busId, (accu1 * x + accu2) `mod` (accu1 * busId))
+  where x = -(offset + accu2) * (accu1 ^ (busId - 2)) `mod` busId
 
 main :: IO ()
 main = do
   (startTime, busIds) <- readInput . lines <$> readFile "input.txt"
   print $ (\(id, time) -> id * (time - startTime)) $ findEarliestBus startTime (map fromJust $ filter (/= Nothing) busIds)
+  print $ snd $ foldl1 findTimestamp (getOffsets busIds)
