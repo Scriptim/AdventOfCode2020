ecc827b149fb807f2104ed3e133cdf9360205bbd
Scriptim <Scriptim@gmx.de>
2020-12-20 16:17:20 +0100

    20.2

--- 20-jurassic_jigsaw/main.hs
+++ 20-jurassic_jigsaw/main.hs
@@ -1,23 +1,28 @@
 import           Control.Monad   (guard)
+import           Data.Bifunctor  (bimap)
 import           Data.List       (nub, transpose)
 import           Data.List.Split (splitOn)
 
-type Tile = (Int, [[Bool]])
+type Image = [[Bool]]
+type Tile = (Int, Image)
 
 parseInput :: String -> [Tile]
 parseInput str = map (parseTile . lines) $ splitOn "\n\n" str
   where parseTile (tileId:tile) = (read . init . last . words $ tileId, map (map (=='#')) tile)
 
-transformations :: Tile -> [Tile]
-transformations (tileId, tile) = do
+transformations :: Image -> [Image]
+transformations image = do
   flip <- [id, map reverse, reverse]
   rotate <- [id, rotateRight 1, rotateRight 2, rotateRight 3]
-  return (tileId, rotate . flip $ tile)
+  return $ rotate . flip $ image
   where rotateRight n = foldl1 (.) (replicate n $ transpose . reverse)
+
+tileTransformations :: Tile -> [Tile]
+tileTransformations (tileId, tile) = map (\t -> (tileId, t)) (transformations tile)
 
 matchRows :: Int -> [Tile] -> [[Tile]]
 matchRows size tiles = nub $ do
-  start <- concatMap transformations tiles
+  start <- concatMap tileTransformations tiles
   let row = matchRow size [start]
   guard $ length row == size
   return row
@@ -43,14 +48,30 @@ matchCols size rows = nub $ do
 nextTiles :: [Tile] -> Tile -> [Tile]
 nextTiles tiles tile = do
   nextTile <- tiles
-  nextTile' <- transformations nextTile
+  nextTile' <- tileTransformations nextTile
   guard $ bordersMatchH tile nextTile'
   return nextTile'
   where bordersMatchH (_, left) (_, right) = map last left == map head right
+
+mergeImage :: [[Tile]] -> Image
+mergeImage = concat . mergeRows . cutBorders
+  where cutBorders = map $ map (map (init . tail) . init . tail . snd)
+        mergeRows = map (foldl1 (zipWith (++)))
+
+findSeaMonsters :: Image -> [[(Int, Int)]]
+findSeaMonsters image = do
+  i <- [0..pred $ length image - maximum (map fst seaMonster)]
+  j <- [0..pred $ length (head image) - maximum (map snd seaMonster)]
+  let indices = map (bimap (+i) (+j)) seaMonster
+  guard $ all (\(i', j') -> image !! i' !! j') indices
+  return indices
+  where seaMonster = [(0, 18)] ++ zip (repeat 1) [0, 5, 6, 11, 12, 17, 18, 19] ++ zip (repeat 2) [1, 4, 7, 10, 13, 16]
 
 main :: IO ()
 main = do
   tiles <- parseInput <$> readFile "input.txt"
   let size = truncate . sqrt . fromIntegral $ length tiles
-      solutions = map (map (map fst)) $ matchCols size $ matchRows size tiles
-  print $ (\sol -> product [head . head $ sol, head . last $ sol, last . head $ sol, last . last $ sol]) $ head solutions
+      solution = head $ matchCols size $ matchRows size tiles
+  print $ (\sol -> product [head . head $ sol, head . last $ sol, last . head $ sol, last . last $ sol]) $ map (map fst) solution
+  let seaMonsterTiles = nub . concat . head . filter (not . null) $ map findSeaMonsters (transformations $ mergeImage solution)
+  print $ length (filter id . concat . mergeImage $ solution) - length seaMonsterTiles
