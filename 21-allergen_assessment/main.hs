98fad335be6359b4dc1389fcae8e1d291e552553
Scriptim <Scriptim@gmx.de>
2020-12-21 11:11:09 +0100

    21.2

--- 21-allergen_assessment/main.hs
+++ 21-allergen_assessment/main.hs
@@ -1,4 +1,6 @@
-import           Data.List       (intersect, nub)
+import           Data.Bifunctor  (second)
+import           Data.List       (intercalate, intersect, nub, partition,
+                                  sortOn)
 import           Data.List.Split (splitOn)
 
 type Food = ([String], [String])
@@ -7,11 +9,19 @@ parseFood :: String -> Food
 parseFood str = (words ingredients, map init . words $ allergenes)
   where [ingredients, allergenes] = splitOn "(contains" str
 
-possiblyAllergic :: [Food] -> [String]
-possiblyAllergic food = nub $ concatMap allergicCandidates (nub $ concatMap snd food)
-  where allergicCandidates allergene = foldl1 intersect (map fst $ filter (elem allergene . snd) food)
+allergenes :: [Food] -> [(String, String)]
+allergenes food = map (second head) . match $ zip ingredients allergenes
+  where
+    allergenes = nub $ map allergicCandidates ingredients
+    allergicCandidates allergene = foldl1 intersect (map fst $ filter (elem allergene . snd) food)
+    ingredients = nub $ concatMap snd food
+    match xs
+      | null ambiguous = unambiguous
+      | otherwise = match $ unambiguous ++ map (second $ filter (`notElem` concatMap snd unambiguous)) ambiguous
+        where (unambiguous, ambiguous) = partition ((==1) . length . snd) xs
 
 main :: IO ()
 main = do
   food <- map parseFood . lines <$> readFile "input.txt"
-  print $ length . filter (`notElem` possiblyAllergic food) $ concatMap fst food
+  print $ length . filter (`notElem` map snd (allergenes food)) $ concatMap fst food
+  print $ intercalate "," . map snd . sortOn fst $ allergenes food
