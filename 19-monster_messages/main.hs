f6365e31be66a5b50309f751c336c246751ae206
Scriptim <Scriptim@gmx.de>
2020-12-21 23:56:28 +0100

    19.1 (optimized)

--- 19-monster_messages/main.hs
+++ 19-monster_messages/main.hs
@@ -1,32 +1,32 @@
-import           Data.Char       (isDigit)
-import           Data.List.Split (splitOn)
-import qualified Data.Map        as M
-import           Data.Maybe      (fromJust)
+import           Data.Char                     (isDigit)
+import           Data.Either                   (isRight)
+import           Data.List.Split               (splitOn)
+import qualified Data.Map                      as M
+import           Data.Maybe                    (fromJust)
+import           Text.ParserCombinators.Parsec (Parser, char, eof, parse, try,
+                                                (<|>))
 
-type Rules = M.Map String [[String]]
+type Rules = M.Map Int [[Either Int String]]
 
 parseInput :: String -> (Rules, [String])
-parseInput str = (M.fromList $ map parseRule rulesRaw, msgsRaw)
+parseInput str = (M.fromList $ map parseRuleLine rulesRaw, msgsRaw)
   where
-    parseRule ruleRaw = (init . head . words $ ruleRaw, splitOn ["|"] . map (filter (/='"')) . tail . words $ ruleRaw)
+    parseRuleLine ruleRaw = (read . init . head . words $ ruleRaw, parseRule . splitOn ["|"] . tail . words $ ruleRaw)
+    parseRule [[rule]] = if isDigit . head $ rule then [[Left $ read rule]] else [[Right $ read rule]]
+    parseRule [rules] = return $ map (Left . read) rules
+    parseRule [rulesA, rulesB] = parseRule [rulesA] ++ parseRule [rulesB]
     [rulesRaw, msgsRaw] = map lines $ splitOn "\n\n" str
 
-validWords :: Rules -> String -> [String]
-validWords rules ruleId
-  | not . isDigit . head . head . head $ rule = head rule
-  | otherwise = do
-      subRule <- rule
-      let nextValidWords = map (validWords rules) subRule
-      if (==1) . length $ nextValidWords
-        then head nextValidWords
-        else do
-          z1 <- head nextValidWords
-          z2 <- head . tail $ nextValidWords
-          return $ z1 ++ z2
-    where rule = fromJust $ M.lookup ruleId rules
-
+matchWord :: Rules -> Parser String
+matchWord rules = parser (fromJust $ M.lookup 0 rules) <* eof
+  where
+    parser [[Right literal]] = return <$> char (head literal)
+    parser [[Left rule]] = lookupParser rule
+    parser [Left ruleA:rules] = try $ (++) <$> lookupParser ruleA <*> parser [rules]
+    parser [rulesA, rulesB] = parser [rulesA] <|> parser [rulesB]
+    lookupParser n = parser . fromJust $ M.lookup n rules
 
 main :: IO ()
 main = do
   (rules, messages) <- parseInput <$> readFile "input.txt"
-  print $ length $ filter (\word -> word `elem` validWords rules "0") messages
+  print $ length $ filter (isRight . parse (matchWord rules) "") messages
